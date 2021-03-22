728d9c702cafb0cb339d8e024b19b202b9a443cf
HHKimryanov19 <HHKimryanov19@codingburgas.bg>
2021-03-22 11:41:59 +0200

    Implement lab4

--- HHKimryanov19/lab4.js
+++ HHKimryanov19/lab4.js
@@ -11,10 +11,13 @@ let sumSuncArrow = (a, b) => a + b;
 let sumSuncArrowWithBody = (a, b) => {
     a++;
     return a + b;
-}
+};
+
 let fooFunc = () => console.log("foo");
 
 console.log(sumSuncArrow.toString());
 console.log(sumFunc(5, 4));
 console.log(sumSuncArrowWithBody(5,4));
+
+fooFunc();
 
