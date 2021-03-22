611fd8947b1e1dd0e912a6cee67f5f8ebd1f0fc0
Marieta Stoycheva <MPStoycheva19@codingburgas.bg>
2021-03-22 11:23:24 +0200

    Implement lab4

---
+++ MPStoycheva19/lab4.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+}
+
+let sumFunc = function(a, b) {
+    return a + b;
+}
+
+let sumSuncArrow = (a, b) => a + b;
+let sumSuncArrowWithBody = (a, b) => {
+    a++;
+    return a + b;
+}
+
+let fooFunc = () => console.log("foo");
+
+// console.log(sumSuncArrow.toSring());
+console.log(sumFunc(5,4));
+console.log(sumSuncArrowWithBody(5,4));
+
+fooFunc();
