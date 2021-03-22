ce10660739bc66e80a07b28daa3d7cb25a6b5282
Zahari Bonev <ZNBonev19@codingburgas.bg>
2021-03-22 11:33:51 +0200

    Implement labZNB5

---
+++ ZNBonev19/labZNB5.js
@@ -0,0 +1,17 @@
+function ask(a, yes, no) {
+    if (a > 18) {
+      yes();
+    } else {
+      no();
+    }
+  }
+  
+  let yesFunc = () => console.log("yes");
+  let noFunc = () => console.log("no");
+  
+  ask(16, yesFunc, noFunc);
+  ask(
+    16,
+    () => console.log("yes"),
+    () => console.log("no")
+  );
