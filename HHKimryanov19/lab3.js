21b06d3a390bbd7b871ac23d75a67a12e0697122
HHKimryanov19 <HHKimryanov19@codingburgas.bg>
2021-03-22 11:22:31 +0200

    Implement lab3

---
+++ HHKimryanov19/lab3.js
@@ -0,0 +1,10 @@
+function product(num1, num2) {
+    return num1 * num2;
+}
+
+let sumFunc = function product(num1, num2) {
+    return num1 * num2;
+};
+
+console.log(sumFunc.toString());
+console.log(sumFunc(5,4));
