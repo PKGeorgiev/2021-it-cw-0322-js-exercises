8858b845021ba35f7676c0bf60827ec519008382
Marieta Stoycheva <MPStoycheva19@codingburgas.bg>
2021-03-22 11:11:32 +0200

    Implement lab2

--- MPStoycheva19/lab1.js
+++ MPStoycheva19/lab1.js
@@ -1,5 +1,10 @@
-function calculate(a, b) {
+function calculate(a, b = 5) {
     return a * b;
 }
 
+function foo(){
+
+}
 console.log(calculate(4,6));
+console.log(calculate(4));
+console.log(foo());
